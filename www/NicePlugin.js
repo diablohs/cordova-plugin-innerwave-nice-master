12988cc4c8b0ef9c75974e2fb15707f11b0ac60d
kimhs <kimhs@innerwave.co.kr>
2020-10-19 15:55:44 +0900

    js fix

--- www/NicePlugin.js
+++ www/NicePlugin.js
@@ -1,5 +1,8 @@
 var exec = require('cordova/exec');
 
-exports.callPayment = function (arg0, success, error) {
-    exec(success, error, 'NicePlugin', 'callPayment', [arg0]);
+NicePlugin.prototype.callPayment = function (arg0, success, error) {
+    cordova.exec(success, error, 'NicePlugin', 'callPayment', [arg0]);
 };
+
+NicePlugin = new NicePlugin();
+module.exports = NicePlugin;
